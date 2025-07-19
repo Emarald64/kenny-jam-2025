26fa65c78781d7d7d4bc1481f288a64b6c1a6c59
Sanikthehedgehorg <168645917+Sanikthehedgehorg@users.noreply.github.com>
2025-07-19 19:33:15 -0400

    Fixed merge issues

---
+++ scripts/character.gd
@@ -0,0 +1,55 @@
+class_name Character
+extends Node2D
+@export var attack = 0
+@export var health = 0
+@export var capacity = 0
+@export var modifiers = [0,0,0,0,0,0]
+const regen = 0
+const rage = 1
+const poison = 2
+const weakness = 3
+const vulnerable = 4
+const paralyze = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func preTurn(playerChars,enemyChars):
+	if modifiers[poison] > 0:
+		health -= modifiers[poison]
+		modifiers[poison] -= 1
+	if modifiers[regen] > 0:
+		health += modifiers[regen]
+		modifiers[regen] -= 1
+func postTurn(playerChars,enemyChars):
+	pass
+func hurt(damage):
+	if modifiers[vulnerable] > 0: 
+		if modifiers[rage] > 0: 
+			health -= (damage + modifiers[rage])*3/2
+			modifiers[rage] -= 1
+		else: health -= damage*3/2
+		modifiers[vulnerable] -= 1
+	else:
+		if modifiers[rage] > 0: 
+			health -= (damage + modifiers[rage])
+			modifiers[rage] -= 1
+		else: health -= damage
+	if health <= 0:
+		queue_free()
+		return true
+	else: return false
+func getAttackDamage():
+	if modifiers[weakness] > 0:
+		modifiers[weakness] -= 1
+		return (attack + rage)*2/3
+	return attack + rage
+func getNumAttacks():
+	if modifiers[paralyze] > 0:
+		modifiers[paralyze] -= 1
+		return 0
+	return 1
