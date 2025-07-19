f5e2298c9b71aabb61775e277c1bd718694ac600
Alex Giller <alexgiller33@gmail.com>
2025-07-19 10:23:49 -0400

    added hand and click affects to cards

--- scripts/card.gd
+++ scripts/card.gd
@@ -4,24 +4,40 @@ class_name Card
 var targetType=Card
 @export var AffectText:String
 var popup
+var popupOn=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func play(target):
+func play():
+	print("Played "+$Title.text)
+	get_parent().addCard(preload("res://scenes/card.tscn").instantiate())
+
+func _process(delta: float) -> void:
+	if Rect2(Vector2(),size).has_point(get_local_mouse_position()):
+		if Input.is_action_just_released("click"):
+			play()
+		if len(AffectText)>0 and not popupOn:
+			popupOn=true
+			popup=preload("res://scenes/popup text.tscn").instantiate()
+			popup.z_index=2
+			popup.get_node("Label").text=AffectText;
+			add_child(popup)
+		scale=Vector2.ONE*1.2
+		position.y=0
+		z_index=1
+	else:
+		if popup!=null:
+			popupOn=false
+			popup.queue_free()
+			#popup=null
+		position.y=10
+		scale=Vector2.ONE
+		z_index=0
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton and event.is_released():
+		#play()
 	pass
-
-
-
-func _on_mouse_entered() -> void:
-	if AffectText!=null:
-		popup=preload("res://scenes/popup text.tscn").instantiate()
-		popup.get_node("Label").text=AffectText;
-		add_child(popup)
-		
-
-
-func _on_mouse_exited() -> void:
-	if popup!=null:
-		popup.queue_free()
---
+++ scripts/hand.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var cards:Array=[]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child in get_children():
+		if child is Card:
+			cards.append(child)
+	print(len(cards))
+	spreadCards()
+
+func spreadCards():
+	for i in range(len(cards)):
+		cards[i].position.x=1000*(i+1)/(len(cards)+1)
+		cards[i].position.y=10
+
+func addCard(card:Card):
+	cards.append(card)
+	add_child(card)
+	spreadCards()
+
+func removeCard(card:Card):
+	cards.erase(card)
+	remove_child(card)
+	spreadCards()
+	
